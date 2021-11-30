797422abf772d85f964f6b359c6c09679c8518d8
yaroslav <yaroslav.polyanskiy@gmail.com>
2021-11-30 17:09:49 +0700

    +

--- 0078.py
+++ 0078.py
@@ -1,3 +1,21 @@
+import datetime
+
 class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
-        
+    def subsets(self, nums):
+        res = [[]]
+        for n in nums:
+            newValues = []
+            for r in res:
+                c = r.copy()
+                c.append(n)
+                newValues.append(c)
+            res.extend(newValues)
+        return res
+
+S = Solution()
+
+t = datetime.datetime.now()
+
+# print(S.subsets([1,2,3,4,5,6,7,8]))
+q = S.subsets(range(20))
+print(datetime.datetime.now() - t)
